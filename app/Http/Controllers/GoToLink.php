121138d1297c95f31144b71dad8d6043ad15adbf
irfans18 <58197653+irfans18@users.noreply.github.com>
2021-11-18 04:43:31 +0700

    fix auth route

--- app/Http/Controllers/GoToLink.php
+++ app/Http/Controllers/GoToLink.php
@@ -7,13 +7,20 @@ use Illuminate\Http\Request;
 
 class GoToLink extends Controller
 {
-    public function go($param){
-      if($param = "dashboard") { 
+   public function go($param)
+   {
+      // dd($param);
+      if ($param =="login") {
+         return redirect()->route('login');
+      }
+      if ($param == "dashboard") {
+         // dd($param);
          return redirect()->route('dashboard');
-      }else
-      $link =  Link::where('name', 'like', '%'.$param.'%')
-                     ->orWhere('cname', 'like', '%'.$param.'%')->first();
-      // dd($link['url']);
-      return redirect($link->url);
-    }
+      } else {
+         $link =  Link::where('name', 'like', '%' . $param . '%')
+            ->orWhere('cname', 'like', '%' . $param . '%')->first();
+         // dd($link['url']);
+         return redirect($link->url);
+      }
+   }
 }
