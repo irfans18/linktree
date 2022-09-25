215b5b32c8f1d4938162a61a128bd0022d022962
irfans18 <nforspeed521@gmail.com>
2022-09-25 23:52:14 +0700

    Configure autoreload
    Add browserSync Script

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,22 @@ const mix = require('laravel-mix');
  |
  */
 
+ // mix.browserSync script
+const domain = 'sherlin.test'; // <= EDIT THIS
+const homedir = require('os').homedir();
+
+// The mix script:
+mix.browserSync({
+      proxy: 'https://' + domain,
+      host: domain,
+      open: 'external',
+      https: {
+      key: homedir + '/.config/valet/Certificates/' + domain + '.key',
+      cert: homedir + '/.config/valet/Certificates/' + domain + '.crt'
+      },
+      notify: true, //Enable or disable notifications
+})
+
 mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
     require('postcss-import'),
     require('tailwindcss'),
